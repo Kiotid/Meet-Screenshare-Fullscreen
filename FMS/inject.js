ca7498ab8eced898dfe6b90ac83d537ac176636f
Samuele Radici <68398653+k-i-o@users.noreply.github.com>
2023-02-16 22:36:09 +0100

    All fine :P

--- FMS/inject.js
+++ FMS/inject.js
@@ -1,8 +1,6 @@
 function setFullScreen() {
-    regularScreen = document.querySelector(".T4LgNb").parentElement.innerHTML;
-    parentScreen = document.querySelector(".T4LgNb").parentElement;
 
-    var trashElements = [document.querySelector(".aGWPv"), document.querySelector(".OGZrkc"), document.querySelector("J0M6X.ZmuLbd.Didmac.G03iKb"), document.querySelector(".lefKC"), document.querySelector(".tTdl5d")]
+    let trashElements = [document.querySelector(".aGWPv"), document.querySelector(".OGZrkc"), document.querySelector("J0M6X.ZmuLbd.Didmac.G03iKb"), document.querySelector(".lefKC"), document.querySelector(".tTdl5d")]
     /*
     if(trashElements[1] != null) trashElements[1].remove(); //chi sta condividendo
     if(trashElements[2] != null) trashElements[2].remove(); //chi sta condividendo
@@ -14,6 +12,7 @@ function setFullScreen() {
     if(trashElements[3] != null) trashElements[3].childNodes.forEach(el=>{el.style.display = "none";}); 
     if(trashElements[4] != null) trashElements[4].setAttribute("jsaction", "");
     if(trashElements[4] != null) trashElements[4].setAttribute("jscontroller", "");
+    if(trashElements[4] != null) trashElements[4].style.display = "none";
     if(trashElements[0] != null) trashElements[0].setAttribute("jscontroller", ""); //avatar
     if(trashElements[0] != null) trashElements[0].style.display = "none";
     
@@ -30,44 +29,97 @@ function setFullScreen() {
     screenSharingStyle.width = "100%";
     screenSharingStyle.height = "100%";
 
-    let bottomBar = document.querySelector(".Sdwpn.P9KVBf").style;
+    let bottomBarParent = document.querySelector(".J0M6X.nulMpf.Didmac.G03iKb").style;
+    bottomBarParent.marginBottom = "1.5vh";
+    bottomBarParent.transform = "scale(.8)";
+
+    let bottomBar = document.querySelector(".UnvNgf.Sdwpn.P9KVBf.mP3Ih").style;
     bottomBar.background = "transparent";
-    bottomBar.marginBottom = "10px";
-    bottomBar.transform = "scale(.8)";
+    
 
     let containerStyle = document.querySelector(".oZRSLe").style;
     containerStyle.width = "100%";
     containerStyle.height = "100%";
 
-    enterFullScreen(document.body);
+    enterFullScreen();
+
 }
 
 function setRegularScreen() {
-    exitFullScreen(document.body);
-    document.querySelector(".T4LgNb").remove();
-    parentScreen.innerHTML = regularScreen;
 
+    zoom(2);
+
+    let trashElements = [document.querySelector(".aGWPv"), document.querySelector(".OGZrkc"), document.querySelector(".J0M6X.ZmuLbd.Didmac.G03iKb"), document.querySelector(".lefKC"), document.querySelector(".tTdl5d")]
+
+    if(trashElements[1] != null) trashElements[1].style.display = "block"; //chi sta condividendo
+    if(trashElements[2] != null) trashElements[2].style.display = "block"; //chi sta condividendo
+    if(trashElements[3] != null) trashElements[3].childNodes.forEach(el=>{el.style.display = "flex";}); 
+    if(trashElements[3] != null) trashElements[3].childNodes.forEach(el=>{el.style.display = "flex";}); 
+    if(trashElements[4] != null) trashElements[4].setAttribute("jsaction", "focusin:zLyIOd; focusout:kLFAfe; mouseenter:pT0VIc; mouseleave:oiMV6; mousedown:JvLOde;c8DrJb:tzZYCf");
+    if(trashElements[4] != null) trashElements[4].setAttribute("jscontroller", "h8UR3d");
+    if(trashElements[4] != null) trashElements[4].style.display = "flex";
+    if(trashElements[0] != null) trashElements[0].setAttribute("jscontroller", "cGKRub"); //avatar
+    if(trashElements[0] != null) trashElements[0].style.display = "block";
+    
+    let screenSharingParent = document.querySelector(".axUSnc.cZXVke.P9KVBf");
+    let screenSharingParentStyle = screenSharingParent.style;
+    screenSharingParent.setAttribute("jsaction", "rcuQ6b:NCu6M; contextmenu:gg8MLb;z5KiKd:qIXZdc;mlNxHf:mDPS3e;ifxHj:mDPS3e;bhyCp:mDPS3e;FzwMpb:mDPS3e;hk5QUe:mDPS3e;Wwt5Kc:mDPS3e;TpIHXe:mDPS3e;EnKPre:eGjA6d;up9WCf:mDPS3e;xj8gtb:mDPS3e;fq58Ob:mDPS3e;IHYwMd:mDPS3e;hENp6e:mDPS3e");
+    screenSharingParentStyle.inset = "72px 16px 80px";
+    screenSharingParentStyle.width = "auto";
+    screenSharingParentStyle.height = "auto";
+
+    let screenSharingStyle = document.querySelector(".dkjMxf").style;
+    screenSharingStyle.left = "0";
+    screenSharingStyle.top = "0";
+    screenSharingStyle.width = "100%";
+    screenSharingStyle.height = "100%";
+
+    let bottomBarParent = document.querySelector(".J0M6X.nulMpf.Didmac.G03iKb").style;
+    bottomBarParent.marginBottom = "0";
+    bottomBarParent.transform = "scale(1)";
+
+    let bottomBar = document.querySelector(".UnvNgf.Sdwpn.P9KVBf.mP3Ih").style;
+    bottomBar.background = "rgb(32,33,36)";
+
+    let containerStyle = document.querySelector(".oZRSLe").style;
+    containerStyle.width = "100%";
+    containerStyle.height = "100%";
+
+    exitFullScreen();
 }
 
 function enterFullScreen() {
-    document.documentElement.requestFullscreen();
+    if(document.documentElement.requestFullscreen)
+        document.documentElement.requestFullscreen();
+    else if(document.documentElement.mozRequestFullScreen)
+        document.documentElement.mozRequestFullScreen();
+    else if(document.documentElement.webkitRequestFullscreen)
+        document.documentElement.webkitRequestFullscreen();
+    else if(document.documentElement.msRequestFullscreen)
+        document.documentElement.msRequestFullscreen();
 };
   
 
-function exitFullScreen(element) {
-	document.exitFullscreen();
+function exitFullScreen() {
+    if(document.exitFullscreen)
+        document.exitFullscreen();
+    else if(document.mozExitFullscreen)
+        document.mozExitFullscreen();
+    else if(document.webkitExitFullscreen)
+        document.webkitExitFullscreen();
+    else if(document.msExitFullscreen)
+        document.msExitFullscreen();
 };
   
 function zoom(inout) {
-    if (zoomValue !== undefined && zoomValue !== null && isFull) {
+    if (isFull) {
 
-        var scrollView = document.querySelector(".koV58.Zi94Db.S7urwe").style;
+        var scrollView = document.querySelector(".koV58.Zi94Db.S7urwe");
 
-        try {
-            document.querySelector(".tTdl5d").remove();
-        } catch { }
+        let focus = document.querySelector(".tTdl5d");
+        if(focus != null) focus.style.display = "none";
 
-        if(scrollView != null) scrollView.overflow = "auto";
+        if(scrollView != null) scrollView.style.overflow = "auto";
 
         if (inout == 0) { //zoomIn
             zoomValue += 50;
@@ -76,7 +128,7 @@ function zoom(inout) {
             zoomValue -= 50;
             console.log("ZoomOut");
         } else if (inout == 2) {
-            if(scrollView != null) scrollView.overflow = "hidden";
+            if(scrollView != null) scrollView.style.overflow = "hidden";
             zoomValue = 2;
             console.log("Reset");
         }
@@ -90,8 +142,6 @@ function zoom(inout) {
     }
 }
 
-var regularScreen;
-var parentScreen;
 var isFull = false;
     
 var zoomValue = 2;
@@ -153,17 +203,18 @@ function onMeetFound(){
     buttonsContainer.insertBefore(zoomout, buttonsContainer.childNodes[6]);
     
     document.querySelector(".fullWidthKio").addEventListener("click", () => {
-        if (canFullScreen) {
-            if (!isFull) {
-                isFull = true;
-                alert("Soon the screenshare will be in fullscreen and you will be able to zoom, to move you can: use the scrollbars, use the trackpad (use the trackpad with two fingers and you can move in all directions), use mouse and keyboard (wheel to scroll up and down, shift+wheel to scroll right and left)");
-                setFullScreen();
-            } else {
-                isFull = false;
-                setRegularScreen();
-            }
-        }else{
+        if (!canFullScreen) {
             alert("No screenshare found!");
+            return;
+        }
+
+        if (!isFull) {
+            isFull = true;
+            alert("Soon the screenshare will be in fullscreen and you will be able to zoom, to move you can: use the scrollbars, use the trackpad (use the trackpad with two fingers and you can move in all directions), use mouse and keyboard (wheel to scroll up and down, shift+wheel to scroll right and left)");
+            setFullScreen();
+        } else {
+            setRegularScreen();
+            isFull = false;
         }
     
     });
@@ -173,19 +224,26 @@ function onMeetFound(){
     document.querySelector(".zoomResetKioBtn").addEventListener("click", () => zoom(2));
 
     setInterval(function () {
-    
-        if (document.querySelector(".axUSnc.cZXVke.P9KVBf").childNodes[0].outerHTML.includes("volume_off")) {
-			try{
-				document.querySelector(".C3LXKe").style.display = "none";
-			}catch{}
-            canFullScreen = true;
-        } else {
-            if(canFullScreen){
-            //    setRegularScreen();
-            }
 
-            canFullScreen = false;
+        let me = document.querySelector(".axUSnc.cZXVke.P9KVBf");
+
+        if(me != null){
+            if (me.childNodes[0].innerHTML.includes("volume_off") && 
+                me.childNodes[0].querySelector(".p2hjYe.TPpRNe").dataset.ssrc != "null") {
+
+                let loopOverlay = document.querySelector(".C3LXKe");
+                if(loopOverlay != null) loopOverlay.style.display = "none";
+                canFullScreen = true;
+            } else {
+                if(canFullScreen && isFull){
+                    setRegularScreen();
+                    isFull = false;
+                }
+
+                canFullScreen = false;
+            }
         }
+
     
     }, 2000);
 }
